c7c68ecab59009dffa17c924d41619d6e454f182
Akanksha Tomar <akanksha4715@gmail.com>
2020-11-11 02:21:00 +0530

    init commit

---
+++ powerset.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void powerr(string s,vector<string> &v,int index=0,string curr =""){
+    int n = s.length();
+    if(index==n){
+        v.push_back(curr);
+        return;
+    }
+    powerr(s,v,index+1,curr);
+    powerr(s,v,index+1,curr+s[index]);
+}
+vector<string> powerset(string &s){
+vector<string> an ; powerr(s,an);
+sort(an.begin(),an.end());
+return an;
+}
+int main(){
+    string s;
+    cin>>s;
+    vector<string> ans = powerset(s);
+    for(auto x:ans){
+        cout<<x<<" ";
+    }
+}
