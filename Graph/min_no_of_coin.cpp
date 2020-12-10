0a478367e4b0925bf841e4aaf39716b5d11d0e59
Akanksha Tomar <akanksha4715@gmail.com>
2020-12-10 16:13:46 +0530

    Coin Chnge

---
+++ Graph/min_no_of_coin.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int minCoins(int arr[],int n,int sum){
+    if(sum==0) return 0;
+    int res=INT16_MAX;
+    for(int i=0;i<n;i++){
+        if(arr[i]<=sum){
+        int sub_res=minCoins(arr,n,sum-arr[i]);
+        if(sub_res<INT16_MAX && sub_res+1<res)
+        res=sub_res+1;
+        }
+    }
+    return res;
+}
+int main(){
+    int coins[] =  {18,1, 10}; 
+    int m = sizeof(coins)/sizeof(coins[0]); 
+    int V = 11; 
+    cout << "Minimum coins required is "
+         << minCoins(coins, m, V); 
+    return 0; 
+}
