e1af9e2d623d4a4423d087bc70f31f7840248c06
Akanksha Tomar <akanksha4715@gmail.com>
2021-03-19 19:31:37 +0530

    P

--- Practice/jump.cpp
+++ Practice/jump.cpp
@@ -1,19 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<bool> visited;
-int fn(int n,vector<int> arr,int s){
+
+bool fn(int n,vector<int> arr,int s){
+    bool x,y;
     if(arr[s]==0)
     return 1;
+    if(s<n && s>0){
     if(arr[s]<s || arr[s]<=n-s){
         if(arr[s]<s && !visited[s]){
             visited[s]=true;
-            return fn(n,arr,s-arr[s]);
+            x= fn(n,arr,s-arr[s]);
         }
         if(arr[s]<=n-s && !visited[s]){
             visited[s]=true;
-            return fn(n,arr,s-arr[s]);
+             y= fn(n,arr,s-arr[s]);
         }
     }
+    }
+    return x|y;
+    
+}
+int main(){
+    int n,s,x;
+    cin>>n;
+    vector<int> v;
+    
+    for(int i=0;i<n;i++){
+        cin>>x;
+        v.push_back(x);
+        visited.push_back(0);
+    }
+    cin>>s;
+    //cout<<visited[0];
+    cout<<fn(n,v,s-1);
     return 0;
 }
-
+// 7
+// 2 1 0 1 2 1 6
+// 6
