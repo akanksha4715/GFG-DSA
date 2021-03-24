337d3fc070632ca9cb91c831a490603b25031355
Akanksha Tomar <akanksha4715@gmail.com>
2021-03-24 19:26:52 +0530

    k

---
+++ Hashing/subarray_sum.cpp
@@ -0,0 +1,30 @@
+//Count no. of subarray wih given sum
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int subarray(int arr[],int target,int n){
+      int sum=0,count=0;
+    unordered_set<int> sett;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if(sum==target)
+        count++;
+        if(sett.find(target-sum)!=sett.end()){
+            count++;
+        }
+        sett.insert(sum);
+        
+    }
+    return count;
+}
+int main(){
+    int n,target;
+    cin>>n>>target;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    cout<<subarray(arr,target,n);
+    return 0;
+}
