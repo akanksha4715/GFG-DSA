ba2abba7d6cfd656e0b4b287580c6b3e9940dfc5
Akanksha Tomar <akanksha4715@gmail.com>
2021-03-26 20:15:54 +0530

    k

---
+++ Hashing/UNION.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+int inter(int a[],int b[],int n,int m){
+    unordered_set<int> sett;
+    for(int i=0;i<n;i++)
+    sett.insert(a[i]);
+    for(int i=0;i<m;i++)
+    {
+        if(sett.find(b[i])==sett.end())
+        {sett.insert(b[i]);
+        n++;}
+    }
+    return n;
+}
+int main(){
+    int m,n;
+    cin>>n>>m;
+    int a[n],b[m];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    for(int i=0;i<m;i++)
+    cin>>b[i];
+    cout<<inter(a,b,n,m);
+    return 0;
+}
