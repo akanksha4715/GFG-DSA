62990cfd3ae0e7fa120fbb1d04baa29a830a248f
Akanksha Tomar <akanksha4715@gmail.com>
2021-03-27 21:12:04 +0530

    k

---
+++ Hashing/LongestSubarray.cpp
@@ -0,0 +1,30 @@
+//Count presence of subarray wih given sum
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int subarray(int arr[],int target,int n){
+      int sum=0,maxlen=0;
+    unordered_map<int,int> sett;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+        if(sum==target)
+        maxlen=max(maxlen,i+1);
+        if(sett.find(sum-target)!=sett.end()){
+            int l=i-sett[sum-target];
+            maxlen=max(maxlen,l);
+        }
+        sett[sum]=i;      
+    }
+    return maxlen;
+}
+int main(){
+    int n,target;
+    cin>>n>>target;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    cout<<subarray(arr,target,n);
+    return 0;
+}
