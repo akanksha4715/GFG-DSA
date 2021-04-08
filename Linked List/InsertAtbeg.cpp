83771512a37d4c0cd40b311bb0649ec314d1661a
Akanksha Tomar <akanksha4715@gmail.com>
2021-04-08 23:28:20 +0530

    LL

---
+++ Linked List/InsertAtbeg.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+node* insertin(node *head,int x){
+    node *temp = new node(x);
+    temp->next=head;
+    //temp->data=x;
+    //head=temp;
+    return temp;
+}
+void print(node *head){
+    node *ptr = NULL;
+    ptr=head;
+    do
+    {
+       cout<<ptr->data<<" ";
+       ptr=ptr->next;
+    }while (ptr->next!=NULL);
+    cout<<ptr->data<<" ";
+}
+int main(){
+    int n,x;
+    cin>>n;
+    cin>>x;
+    node *head=new node(x);
+    //cout<<"u";
+    head->next=NULL; 
+    for(int i=1;i<n;i++){       
+        cin>>x;
+        head=insertin(head,x);
+    }
+    print(head);
+}
