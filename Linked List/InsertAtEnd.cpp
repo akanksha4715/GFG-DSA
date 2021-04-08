8666ee5ad9a225273840c4ecce6325c6c564f91d
Akanksha Tomar <akanksha4715@gmail.com>
2021-04-09 01:31:46 +0530

    LL

--- Linked List/InsertAtEnd.cpp
+++ Linked List/InsertAtEnd.cpp
@@ -1,19 +1,51 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 struct node{
     int data;
     node *next;
-    node(int n){
-        data=n;
+    node(int x){
+        data=x;
         next=NULL;
     }
 };
-int main(){
-    //cout<<"Enter no of nodes";
-    node *n1= new node(10);
-    node n2=node(2);
-    cout<<n2.data;
-    cout<<n1->data;
-    return 0;
-
+void insertin(node *head,int x){
+    node *temp = new node(x);
+    temp->next=NULL;
+    node *ptr = head;    
+    while (ptr->next!=NULL)
+       ptr=ptr->next;
+     ptr->next=temp; 
 }
+void print(node *head){
+    node *ptr = NULL;
+    ptr=head;
+    do
+    {
+       cout<<ptr->data<<" ";
+       ptr=ptr->next;
+    }while (ptr->next!=NULL);
+    cout<<ptr->data<<" ";
+}
+int main(){
+    int n,x;
+    cin>>n;
+    cin>>x;
+    node *head=new node(x);
+    head->next=NULL; 
+    for(int i=1;i<n;i++){       
+        cin>>x;
+        insertin(head,x);
+    }
+    print(head);
+}
+
+// int main(){
+//     //cout<<"Enter no of nodes";
+//     node *n1= new node(10);
+//     node n2=node(2);
+//     cout<<n2.data;
+//     cout<<n1->data;
+//     return 0;
+
+// }
