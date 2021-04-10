3c0b02367119bc6a590ef23c830f15c00ff676e2
Akanksha Tomar <akanksha4715@gmail.com>
2021-04-10 17:32:00 +0530

    g

--- Practice/Course_schedule.cpp
+++ Practice/Course_schedule.cpp
@@ -1,18 +1,14 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-bool isSafe(){
-
-}
-bool dfs(bool visited[],bool recst[],vector<vector<int>>& prerequisites,int src){
+bool dfs(bool visited[],bool recst[],vector<int> adj[],int src){
     visited[src]=1;
     recst[src]=1;
-    for(int u:prerequisites[src]){
-        //int u=prerequisites[i][1];
+    for(int u:adj[src]){
         if(!visited[u]){
             visited[u]=1;
             recst[u]=1;
-            if(dfs(visited,recst,prerequisites,u))
+            if(dfs(visited,recst,adj,u))
             return true;
         }
         else if(recst[u])
@@ -23,6 +19,10 @@ bool dfs(bool visited[],bool recst[],vector<vector<int>>& prerequisites,int src)
 }
 bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
     int n=numCourses;
+    vector<int> adj[n];
+    for(int i=0;i<prerequisites.size();i++){
+        adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+    }
     bool visited[n],recst[n];
     for(int i=0;i<n;i++){
         visited[i]=0;
@@ -30,11 +30,9 @@ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
     }
         for(int i=0;i<n;i++)
         if(!visited[i]){
-         if(dfs(visited,recst,prerequisites,i))
+         if(dfs(visited,recst,adj,i))
          return false;
         }
         return true; 
     }
-int main(){
 
-}
---
+++ Practice/CrseSchedule.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// bool isSafe(){
+
+// }
+// bool dfs(bool visited[],bool recst[],vector<vector<int>>& prerequisites,int src){
+//     visited[src]=1;
+//     recst[src]=1;
+//     for(int u:prerequisites[src]){
+//         //int u=prerequisites[i][1];
+//         if(!visited[u]){
+//             visited[u]=1;
+//             recst[u]=1;
+//             if(dfs(visited,recst,prerequisites,u))
+//             return true;
+//         }
+//         else if(recst[u])
+//         return true;
+//     }
+//     recst[src]=0;
+//     return false;
+// }
+// bool canFinish(int n, vector<vector<int>>& prerequisites) {
+    
+
+//     }
+int main(){
+vector<int> v[3];
+cout<<"hi";
+	v[0].push_back(1);
+    cout<<v[0][1];
+	v[1].push_back(3);
+	
+	
+    int n;
+	cout<<n;
+}
