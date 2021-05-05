c130e810b36e68348ff845c9eaf90c7c76e4ad08
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-05 15:27:15 +0530

    fla

---
+++ Practice/SparseMatrix.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class SparseMatrix
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+    }
+    
+}
