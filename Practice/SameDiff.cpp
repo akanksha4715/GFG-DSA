5404b495fb439f16abac45cdd56609b288553291
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-06 22:16:40 +0530

    code

---
+++ Practice/SameDiff.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t){
+    t--;
+    int n,j,count=0;
+    cin>>n;
+    vector<int> vec;
+    map<int,int> sett;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        j=x-i;
+        count+=sett[j];
+        sett[j]++;
+    }
+    cout<<count<<endl;
+    }
+    return 0;
+}
