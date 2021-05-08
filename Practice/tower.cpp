ecd0c16b31cbb3271130a6c04071db1a3db009c9
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-08 19:37:28 +0530

    code

---
+++ Practice/tower.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> vec(n);
+    for(int i=0;i<n;i++)
+    cin>>vec[i];
+    sort(vec.begin(),vec.end(),greater<int>());
+    int k=0,i=1;
+    int maxx=vec[0];
+    cout<<"==============";
+    for(i=1;i<=maxx;i++){
+        k++;
+        if(i+vec[k]<maxx)
+        maxx=i+vec[k];
+        //cout<<i;
+    }
+    cout<<endl<<i;
+    return 0;
+}
