3b0cc4352b6de99221dbadbf583342883451a8ae
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-13 18:40:58 +0530

    code

--- Practice/NoOfIslands.cpp
+++ Practice/NoOfIslands.cpp
@@ -1,28 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isSafe(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j,int count){
+bool isSafe(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j){
         int n=grid.size();
+        int count=grid[0].size();
         return ( i>=0 && j>=0 && i<n && j<count && !visited[i][j] && grid[i][j]=='1' );
     }
-    void bfs(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j,int count){
+    void bfs(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j){
         int n=grid.size();
+        int count=grid[0].size();
         int row[4]={0,-1,0,1};
         int col[4]={-1,0,1,0};
         visited[i][j]=1;
         queue<pair<int,int>> q;
         q.push(make_pair(i,j));
         while(!q.empty()){
-            cout<<"two";
             int u=q.front().first;
             int v=q.front().second;
-            visited[u][v]=1;
             q.pop();
             for(int k=0;k<4;k++){
-                if(isSafe(grid,visited,u+row[k],v+col[k],count)){
-                    cout<<"3";
-                    q.push( make_pair(u+row[k],v+row[k]) );
+                if(isSafe(grid,visited,u+row[k],v+col[k])){
+                    visited[u+row[k]][v+col[k]]=1;
+                    q.push( make_pair(u+row[k],v+col[k]) );
                 }
             }
+
         }
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -40,8 +41,9 @@ bool isSafe(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j
         for(int i=0;i<n;i++){
             for(int j=0;j<count;j++){
                 if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(grid,visited,i,j,count);
+                    bfs(grid,visited,i,j);
                     ans++;
+                    //cout<<i<<" "<<j<<" "<<ans<<endl;
                 }
             }
         }
@@ -50,10 +52,8 @@ bool isSafe(vector<vector<char>>& grid,vector<vector<bool>>& visited,int i,int j
     int main(){
         vector<vector<char>> grid={
             {'1','1','1','1','0'},
-            {'1','1','1','1','0'},
-            {'1','1','1','1','0'},
-            {'1','1','1','1','0'},
-            {'1','1','1','1','0'}
+            {'1','1','0','1','0'},
+            {'1','1','0','0','0'}
         };
     cout<<numIslands(grid);
         return 0;
