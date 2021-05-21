3b89f7c2e44658b7b1e5fd9935003bb6b702ccb7
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-21 20:31:41 +0530

    code

--- Practice/Count_CombinationSum.cpp
+++ Practice/Count_CombinationSum.cpp
@@ -3,27 +3,7 @@
 using namespace std;
 vector<int> coins;
 int fn(int target,int i,int n){
-    int count=0;
-    if(target==0)
-    return 0;
-    else if(coins[i]-target==0)
-    return 1;
-    else if(target-coins[i]<0)
-    return -1;
-    else{
-        for(int j=0;j<n;j++){
-            int ans;
-            if(target-coins[j]==0)
-            ans=1;
-            else
-            ans=fn(target-coins[j],j,n);
-
-            cout<<ans<<"for target"<<target-coins[j]<<"at"<<j<<endl;
-            if(ans!=-1)
-            count+=ans;
-        }
-    }
-    return count;
+    
 }
 int main(){
     int n=4,count=0;
@@ -37,7 +17,6 @@ int main(){
     coins=vec;
     for(int i=0;i<n;i++){
         int ans;
-        cout<<"at "<<i<<endl;
         if(target-coins[i]==0)
         ans=1;
         else
