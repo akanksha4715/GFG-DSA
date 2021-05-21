0c167223af8637d2edc77ac07220284484b2f1df
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-22 00:53:34 +0530

    code

--- Practice/Count_CombinationSum.cpp
+++ Practice/Count_CombinationSum.cpp
@@ -1,9 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<set>
 using namespace std;
-vector<int> coins;
-int fn(int target,int i,int n){
-    
+vector<vector<int>> res;
+set<vector<int>> sett;
+//2 3 6 7 
+//7
+void solve(vector<int>& candidates, int target,vector<int> &ans){
+    if(target==0){
+        vector<int> temp=ans;
+        sort(temp.begin(),temp.end());
+        if(sett.find(temp)==sett.end()){
+            res.push_back(temp);
+            sett.insert(temp);
+        }
+        
+    }
+    else{
+
+        for(int x:candidates){
+            if(target-x>=0){
+            ans.push_back(x);
+            solve(candidates,target-x,ans);
+            ans.pop_back();
+            }
+        }
+    }
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target){
+    vector<int> answer;
+    sort(candidates.begin(),candidates.end());
+    solve(candidates,target,answer);
+    return res;
 }
 int main(){
     int n=4,count=0;
@@ -14,17 +43,7 @@ int main(){
         cin>>x;
         vec.push_back(x);
     }
-    coins=vec;
-    for(int i=0;i<n;i++){
-        int ans;
-        if(target-coins[i]==0)
-        ans=1;
-        else
-        ans=fn(target-coins[i],i,n);
-        
-        if(ans!=-1)
-        count+=ans;
-    }
-    cout<<endl<<count;
+    vector<vector<int>> res=combinationSum(vec,target);
+    cout<<res.size();
     return 0;
 }
