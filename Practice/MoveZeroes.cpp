03b48ebc4ccfb1d63933c89a12283a59bd507554
Akanksha Tomar <akanksha4715@gmail.com>
2021-05-29 00:35:24 +0530

    code

---
+++ Practice/MoveZeroes.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //int n=nums.size();
+        int ptr=0;
+        for(int i=0;i<nums.size();i++)
+            if(nums[i]!=0)
+            swap(nums[ptr++],nums[i]);
+    }
+};
