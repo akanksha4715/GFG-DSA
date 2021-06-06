123119561fdd3b521b29e3b1204b62489e6afaf3
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-06 23:34:32 +0530

    code

--- Matrix/SpiralMatrix.cpp
+++ Matrix/SpiralMatrix.cpp
@@ -3,22 +3,41 @@ using namespace std;
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m=matrix.size(); int n=matrix[0].size();
+        int m=matrix.size(),n=matrix[0].size();
         vector<int> ans;
-        int topleft=0,topright=n-1,bottomleft=m-1,bottomright=n-1;
-        while(topleft<bottomleft && topright<bottomright){
-            for(int i=topleft;i<=topright;i++){
+        int leftcol=0,rightcol=n-1;int count=0;
+        
+        int topleft=0,bottomleft=m-1;
+        while(topleft<=bottomleft && rightcol>=leftcol){
+            //cout<<count++<<" ";
+            for(int i=topleft;i<=rightcol;i++){
+                
                 ans.push_back(matrix[topleft][i]);
             }
-            topleft++;topright++;
-            for(int i=topright;i<=bottomright;i++){
-                ans.push_back(matrix[i][topright]);
+            
+            topleft++;
+            
+            for(int i=topleft;i<=bottomleft;i++){
+                ans.push_back(matrix[i][rightcol]);
             }
-            topright--;bottomright--;
-            for(int i=bottomright;i<=bottomleft;i++){
-                ans.push_back(1);
+            
+            rightcol--;
+            if(topleft<=bottomleft){
+            for(int i=rightcol;i>=leftcol;i--){
+                //cout<<"in"<<" ";
+                ans.push_back(matrix[bottomleft][i]);
+            }
+            
+            bottomleft--; 
+            }
+            if(leftcol<=rightcol){
+            for(int i=bottomleft;i>=topleft;i--){
+                ans.push_back(matrix[i][leftcol]);
+            }
+            
+            leftcol++; 
             }
         }
-        
+        return ans;
     }
 };
