8e3ac78305b4b9a633c30592013860d7e9c49a41
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-07 01:38:29 +0530

    code

--- Matrix/WordSearch.cpp
+++ Matrix/WordSearch.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 class Solution {
 public:
-    bool isSafe(int x,int y,char w,vector<vector<char>> &board,vector<vector<bool>> &visited){
+    bool isSafe(int x,int y,char w,vector<vector<char>> &board){
         int m=board.size();
         int n=board[0].size();
-        return x>=0 && x<m && y>=0 && y<n && board[x][y]==w && !visited[x][y];
+        return x>=0 && x<m && y>=0 && y<n && board[x][y]==w;
     }
-    bool search(vector<vector<char>> &board,vector<vector<bool>> &visited,string w,int si,int sj,int index){
+    bool search(vector<vector<char>> &board,string w,int si,int sj,int index){
         if(index==w.length())
             return true;
         int row[]={0,-1,0,1};
@@ -15,12 +15,12 @@ public:
         for(int i=0;i<4;i++){
             int x=si+row[i];
             int y=sj+col[i];
-            if(isSafe(x,y,w[index],board,visited)){
-                visited[x][y]=1;
-                if(search(board,visited,w,x,y,index+1))
+            if(isSafe(x,y,w[index],board)){
+                board[x][y]='*';
+                if(search(board,w,x,y,index+1))
                     return true;
                 else
-                    visited[x][y]=0;
+                    board[x][y]=w[index];
             }
             
         }
@@ -29,18 +29,14 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size();
         int n=board[0].size();
-        vector<vector<bool>> visited(m);
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-                visited[i].push_back(0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                  visited[i][j]=1;
-                    if(search(board,visited,word,i,j,1))
+                  board[i][j]='*';
+                    if(search(board,word,i,j,1))
                         return true;
                     else
-                        visited[i][j]=0;
+                        board[i][j]=word[0];
                 }
             }
         }
