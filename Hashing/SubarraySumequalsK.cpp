250261cf51dc477dcd2a556d5884e18d2a8b1e58
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-08 00:41:16 +0530

    code

---
+++ Hashing/SubarraySumequalsK.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int sum=0,count=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==k || mp.find(sum-k)!=mp.end()){
+                count+=mp[sum-k];
+            }
+                mp[sum]++;
+            
+        }
+        return count;
+    }
+};
