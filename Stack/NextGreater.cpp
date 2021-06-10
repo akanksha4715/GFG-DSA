0f88c334712984b3d9d73e1f0ed09b06a57c59af
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-11 02:00:28 +0530

    code

--- Stack/NextGreater.cpp
+++ Stack/NextGreater.cpp
@@ -2,15 +2,30 @@
 #include<stack>
 #include<vector>
 using namespace std;
-vector<int> findans(vector<int> arr){
+deque<int> findans(vector<int> arr){
     int n=arr.size();
     stack<int> st;
-    st.push(arr[0]);
+    deque<int> ans;
+    st.push(arr[n-1]);
+    ans.push_front(-1);
+    //cout<<-1<<" ";
     for(int i=n-2;i>=0;i--){
-
+        while(!st.empty() && st.top()<=arr[i])
+        st.pop();
+        if(st.empty())
+        ans.push_front(-1);
+        //cout<<-1<<" ";
+        else
+        //cout<<st.top()<<" ";
+        ans.push_front(st.top());
+        st.push(arr[i]);
     }
+    return ans;
 }
 int main(){
     vector<int> arr={5,15,10,8,6,12,18};
-    vector<int> vec=findans(arr);
+    deque<int> vec=findans(arr);
+    for(auto x:vec)
+    cout<<x<<" ";
+    
 }
