9be4827ccfb8cb939cf75d939e1c5c126ed38eba
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-10 23:22:16 +0530

    code

---
+++ Stack/NextGreater.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+vector<int> findans(vector<int> arr){
+    int n=arr.size();
+    stack<int> st;
+    st.push(arr[0]);
+    for(int i=n-2;i>=0;i--){
+
+    }
+}
+int main(){
+    vector<int> arr={5,15,10,8,6,12,18};
+    vector<int> vec=findans(arr);
+}
