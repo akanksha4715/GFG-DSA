3e86050298c193c68204b08e8618c2bdd2965267
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-15 00:16:32 +0530

    code

--- Practice/Walmart.cpp
+++ Practice/Walmart.cpp
@@ -1,53 +1,78 @@
 #include<string.h>
 #include <cmath>
-#include<cstdio>
-#include<vector>
+#include <cstdio>
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include<unordered_set>
 using namespace std;
-void solve(vector<int>& P, int n)
+
+unordered_set<string> printAllKLengthRec(string str, string prefix,int n, int k,unordered_set<string> sett)
+                                    
 {
- 
-    // New array to convert
-    // to 1-based indexing
-    vector<int> arr;
- 
-    arr.push_back(0);
- 
-    for (auto x : P)
-        arr.push_back(x);
- 
-    // Keeps count of swaps
-    int cnt = 0;
- 
-    for (int i = 1; i < n; i++) {
- 
-        // Check if it is an 'X' position
-        if (arr[i] == i) {
-            swap(arr[i], arr[i + 1]);
-            cnt++;
-        }
+     
+    if (k == 0)
+    {   //cout<<prefix<<endl;
+        sort(prefix.begin(), prefix.end());
+        sett.insert(prefix);
+        return sett;
     }
- 
-    // Corner Case
-    if (arr[n] == n) {
- 
-        swap(arr[n - 1], arr[n]);
-        cnt++;
+    for (int i = 0; i < n; i++)
+    {
+        string newPrefix;
+        newPrefix = prefix + str[i];
+        sett=printAllKLengthRec(str, newPrefix, n, k - 1,sett);
     }
+    return sett;
  
-    // Print the minimum swaps
-    cout << cnt << endl;
 }
-int main(){
-    int n;
-    cin>>n;
-    vector<int> P;
-    for(int i=0;i<n;i++){
+ 
+
+void query1(string s,int l,int r,int k){
+    unordered_set<string> sett;
+    string str=s.substr(l-1,r-l+1);
+    int len=str.length();
+    sett=printAllKLengthRec(str, "", len, k,sett);
+    cout<<sett.size()<<endl;
+    
+}
+string query2(string s,int p,char c){
+    s[p-1]=c;
+    return s;
+}
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n,q;
+    cin>>n>>q;
+    string s;
+    cin>>s;
+    int l,r,k;
+    int p;
+    char c;
+    for(int i=0;i<q;i++){
         int x;
         cin>>x;
-        P.push_back(x);
+        if(x==1){
+            
+            cin>>l>>r>>k;
+            query1(s,l,r,k);
+        }
+        else{
+            
+            cin>>p;
+            cin>>c;
+           s= query2(s,p,c);
+        }
+        
     }
-    solve(P,n);
+
+    return 0;
 }
+/*
+8 4
+elemente
+1 1 3 2
+2 3 b
+1 1 3 2
+1 1 8 9
+*/
