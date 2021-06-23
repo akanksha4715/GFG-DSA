cf1387c0d39702d48df417d37475869cf35481ff
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-24 01:34:43 +0530

    code

--- Practice/trial.cpp
+++ Practice/trial.cpp
@@ -1,36 +1,25 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-
-class Solution {
-public:
-    int coinChange(vector<int>& coins, int sum) {
-        int n=coins.size();
-        int dp[n][sum+1];
-        for(int i=0;i<n;i++)
-            dp[i][0]=0;
-        
-        
-        for(int i=1;i<=sum;i++){
-            for(int j=1;j<n;j++){
-                if(i<coins[j])
-                    dp[i][j]=dp[i-1][j];
-                else
-                    
-                dp[i][j]=min(dp[i-coins[j]][j]+1,dp[i-1][j]);
-            }
-        }
-        int ans= dp[n-1][sum];
-        if(ans==INT16_MAX)
-            return -1;
-        else 
-            return ans;
-    }
-};
-int main(){
-    vector<int> v1={2,5,8};
-    vector<int> v2={2,5,8};
-    cout<<(v1==v2);
-    return 0;
-}
+#include <iostream>  
+using namespace std;  
+class Employee {  
+   public:  
+       int id; //data member (also instance variable)      
+       string name; //data member(also instance variable)  
+       float salary;  
+       Employee(int id, string name, float salary)    
+        {    
+             this->id = id;    
+            this->name = name;    
+            this->salary = salary;   
+        }    
+       void display()    
+        {    
+            cout<<id<<"  "<<name<<"  "<<salary<<endl;    
+        }    
+};  
+int main(void) {  
+    Employee e1 =Employee(101, "Sonoo", 890000); //creating an object of Employee   
+    Employee e2=Employee(102, "Nakul", 59000); //creating an object of Employee  
+    e1.display();    
+    e2.display();    
+    return 0;  
+}  
