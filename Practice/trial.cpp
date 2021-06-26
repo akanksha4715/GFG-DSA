3e06a090c2e0ee53cb712a8898cd454023373844
Akanksha Tomar <akanksha4715@gmail.com>
2021-06-26 14:44:31 +0530

    code

--- Practice/trial.cpp
+++ Practice/trial.cpp
@@ -19,7 +19,11 @@ class Employee {
 int main(void) {  
     Employee e1 =Employee(101, "Sonoo", 890000); //creating an object of Employee   
     Employee e2=Employee(102, "Nakul", 59000); //creating an object of Employee  
-    e1.display();    
-    e2.display();    
+   // e1.display();    
+   // e2.display();    
+   string a="12rt56";
+   for(int i=0;i<a.length();i++)
+   if(a[i]-'0'>=0 && a[i]-'0'<=9)
+   cout<<a[i];
     return 0;  
 }  
